be632dd0ae50aeb0e770fe38845d89b218d45334
Juliano Sill <sill.juliano@gmail.com>
2022-12-30 22:21:21 -0300

    feat(login()): create a function to check and log in

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+/* For testing purposes */
+const correctEmail = 'admin@a.com';
+const correctPassword = 'pass123';
+
+
 const loginForm = document.querySelector('.login-form');
 
 const login = () => {
-	const email = document.querySelector('#email');
-	const password = document.querySelector('#password');
+	const email = document.querySelector('#email').value;
+	const password = document.querySelector('#password').value;
 	const warning = document.querySelector('.form-warning');
+	if(email === correctEmail && password === correctPassword) {
+		alert('Sucess!');
+	} else if(email !== correctEmail) {
+		warning.textContent = 'This email is not registered. Please, try again.';
+	} else if(password !== correctPassword) {
+		warning.textContent = 'Password incorrect! Please, try again.';
+	} else {
+		warning.textContent = 'Error. Please, contact the support team.';
+	};
 
-
-	console.log('Login attempted!')
 }
 
 // Prevent the default behavior of the submit event (page refresh)
