cafa07b05dcc3e7b9d2672c9e11ebcfc1403e596
Juliano Sill <sill.juliano@gmail.com>
2023-01-03 21:31:04 -0300

    feat(loginPromise): create the login promise

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ const loginForm = document.querySelector('.login-form');
 const warning = document.querySelector('.form-warning');
 const loginButton = document.querySelector('.login-btn');
 
+// Checks user data, simulates a waiting time with a promise that returns the result after it
+const loginPromise = () => {
+	// Gets the value after the button is clicked
+	const email = document.querySelector('#email').value;
+	const password = document.querySelector('#password').value;
+	// Disables the button during the attempt
+	loginButton.setAttribute('disabled', '');
+
+	return new Promise((resolve, reject) => {
+		// setTimeout() simulates requesting and waiting the server response
+		setTimeout(() => {
+			if(email === correctEmail && password === correctPassword) {
+				resolve('You\'re logged in!');
+			} else {
+				reject('Email or password incorrect. Please, try again.')
+			}
+		}, 1000);
+	});
+};
+
 // Prevent the default behavior of the submit event (page refresh)
 loginForm.addEventListener("submit", function(event) {
 	event.preventDefault();
