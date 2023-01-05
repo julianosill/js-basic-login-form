938d9c39cf8a72e41329c4309309ee9b0f63c513
Juliano Sill <sill.juliano@gmail.com>
2023-01-04 21:39:39 -0300

    feat(loginPromise()): add an object to return why login failed

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const correctPassword = 'pass123';
 
 // Global variables
 const loginForm = document.querySelector('.login-form');
-const warning = document.querySelector('.form-warning');
 const loginButton = document.querySelector('.login-btn');
+const warning = document.querySelector('.form-warning');
+// An object to get user email and password after login button is clicked
+const userData = {};
 
 // Checks user data, simulates a waiting time with a promise that returns the result after it
 const loginPromise = () => {
-	// Gets the value after the button is clicked
-	const email = document.querySelector('#email').value;
-	const password = document.querySelector('#password').value;
+	// Gets the value after the button is clicked and stores it in two properties
+	userData.email = document.querySelector('#email').value;
+	userData.password = document.querySelector('#password').value;
 	// Disables the button during the attempt
 	loginButton.setAttribute('disabled', '');
 
@@ -21,10 +23,10 @@ const loginPromise = () => {
 	return new Promise((resolve, reject) => {
 		// setTimeout() simulates requesting and waiting the server response
 		setTimeout(() => {
-			if(email === correctEmail && password === correctPassword) {
+			if(userData.email === correctEmail && userData.password === correctPassword) {
 				resolve('You\'re logged in!');
 			} else {
-				reject('Email or password incorrect. Please, try again.')
+				reject('Email or password incorrect.');
 			}
 		}, delay);
 	});
@@ -36,8 +38,14 @@ const loginSuccessful = (successfulMessage) => {
 };
 
 // Gets the failed message from the promise, then shows it in the ".warning" div
-const loginFailed = (failedMessage) => {
-	warning.textContent = failedMessage;
+const loginFailed = () => {
+	if(userData.email !== correctEmail) {
+		warning.textContent = 'Email is not valid. Please, try again';
+	} else if (userData.password !== correctPassword) {
+		warning.textContent = 'Password is incorrect. Please, try again.'
+	} else {
+		warning.textContent = 'Login failed. Please, contact our support team.'
+	};
 };
 
 const enableLoginButton = () => {
